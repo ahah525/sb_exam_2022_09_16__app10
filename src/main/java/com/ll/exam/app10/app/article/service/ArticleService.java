e3f053b54e9a5fbc055769feb2bbecabe99ccd9f
ahah525 <ahah525@naver.com>
2022-09-20 00:36:30 +0900

    feat : dev 모드 초기 데이터 게시글 1개(이미지 2개) 생성

--- src/main/java/com/ll/exam/app10/app/article/service/ArticleService.java
+++ src/main/java/com/ll/exam/app10/app/article/service/ArticleService.java
@@ -2,6 +2,7 @@ package com.ll.exam.app10.app.article.service;
 
 import com.ll.exam.app10.app.article.entity.Article;
 import com.ll.exam.app10.app.article.repository.ArticleRepository;
+import com.ll.exam.app10.app.fileUpload.service.GenFileService;
 import com.ll.exam.app10.app.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,17 +11,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+    private final GenFileService genFileService;
 
     public Article write(Long authorId, String subject, String content) {
+        return write(new Member(authorId), subject, content);
+    }
+
+    public Article write(Member author, String subject, String content)  {
         Article article = Article.builder()
                 .subject(subject)
                 .content(content)
-                .author(new Member(authorId))
+                .author(author)
                 .build();
 
         return articleRepository.save(article);
     }
     public Article getArticleById(Long id) {
         return articleRepository.findById(id).orElse(null);
+    }
+
+    public void addGenFileByUrl(Article article, String typeCode, String type2Code, int fileNo, String url) {
+        genFileService.addGenFileByUrl("article", article.getId(), typeCode, type2Code, fileNo, url);
     }
 }
